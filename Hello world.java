0c4d42bb313c67380d2eb6ec65b8398306e1a120
ed7272 <58631334+ed7272@users.noreply.github.com>
2019-12-08 11:00:38 +0000

    GCI

--- Hello world.java
+++ Hello world.java
@@ -2,7 +2,7 @@ package net.codejava;
 
 public class Main{
     public static void main(String{} args){
-        System.out.printLn("Hello World");
+        System.out.printLn("Hello World, this is GCI");
     }
 
 }
